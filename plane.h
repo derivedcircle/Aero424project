9368b3907b14fd84f422bdee334c95f983d946d2
djk6149 <91642380+djk6149@users.noreply.github.com>
2024-04-11 12:03:58 -0400

    changes to plane.h

--- plane.h
+++ plane.h
@@ -1,7 +1,15 @@
+#include <iostream>
 #ifndef plane_H
 #define plane_H
 //include guards
 
-
+class plane
+{
+	virtual void getIssue() = 0;
+	virtual void possibleSolutions() = 0;
+	virtual void getSize() = 0;
+	virtual void costImplentation() = 0;
+	virtual ~plane();
+};
 
 #endif // !plane_H
