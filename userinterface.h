b26865f736c46bbfddcb8bba31f682ef4bd671e3
AnshS <shahansh0509@gmail.com>
2024-04-11 12:05:12 -0400

    started work on the user interface class abstraction

--- userinterface.h
+++ userinterface.h
@@ -3,6 +3,7 @@
 // include guard
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -10,7 +11,13 @@ class UserInterface
 {
 	string username;
 	double hangarSpace;
-
+	float moneySpent;
+	int days;
+	// a map with the planes in the hanger and how many days it will take to do their maintanance
+public:
+	UserInterface( string name);
+	void displayCurrStatus();// function that displays the current status of the aircraft in the hangar (replace with an opperator override)
+	void newAircraftArrivals();// function that creates new aircraft to pick from 
 };
 
 #endif // !UI_H
