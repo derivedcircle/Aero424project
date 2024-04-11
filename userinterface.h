4acc05f5cb6ae193b113ea368f248af152285787
AnshS <shahansh0509@gmail.com>
2024-04-10 21:21:12 -0400

    updates on userinterface class

--- userinterface.h
+++ userinterface.h
@@ -1,6 +1,16 @@
 #ifndef UI_H
 #define UI_H
 // include guard
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class UserInterface
+{
+	string username;
+	double hangarSpace;
+
+};
 
 #endif // !UI_H
