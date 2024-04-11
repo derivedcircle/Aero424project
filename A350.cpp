b9abef509763134df4006c3a26ce8f39baa48b14
djk6149 <91642380+djk6149@users.noreply.github.com>
2024-04-11 19:28:14 -0400

    changes to plane.h and plane model .h files

---
+++ A350.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include "initializer.h"
+#include "instructions.h"
+#include "plane.h"
+#include "userinterface.h"
+#include "A350.h"
+
+
+//problems the A350 plane can have
+//user chooses number and based off of that, a problem is generated
+void A350IssueandSolutions::getIssue(int num) {
+	switch (num) {
+	case 0:
+		std::cout << "The tire fell off during flight" << endl;
+		break;
+	case 1:
+		std::cout << "There is no fuel" << endl;
+		break;
+	}
+	while (num != 1 || num != 0)
+	{
+		std::cout << "Please enter either a 1 or 0";
+	}
+}
+
+//user must decide on the best course of action to fix the problem above
+void A350IssueandSolutions::possibleSolutions(int number) {
+	char options;
+	switch (number) {
+		//Issue: Engine filled with diesel
+	case 0:
+		std::cout << "How do you want to solve this issue? Press A to replace the wheel and bolts ($500). Press B to do nothing ($0). Press C to replace the door ($400)" << endl;
+		std::cin >> options;
+		switch (options) {
+		case 'A':
+			time = 7;
+			cost = 500;
+			issueFixed = true;
+			break;
+		case 'B':
+			time = 0;
+			cost = 0;
+			issueFixed = false;
+			break;
+		case 'C':
+			time = 7;
+			cost = 500;
+			issueFixed = false;
+			break;
+		}
+		break;
+	case 1:
+		//No fuel
+		std::cout << "How do you want to solve the issue? Press A to refuel ($50). Press B to do nothing ($0). Press C to replace the whole engine ($500)" << endl;
+		std::cin >> options;
+		switch (options) {
+		case 'A':
+			time = 0;
+			cost = 50;
+			issueFixed = true;
+			break;
+		case 'B':
+			time = 0;
+			cost = 0;
+			issueFixed = false;
+			break;
+		case 'C':
+			time = 7;
+			cost = 500;
+			issueFixed = true;
+			break;
+		}
+		while (options != 'A' || options != 'B' || options != 'C')
+		{
+			cout << "Please choose A,B, or C depending on which option you think is best.";
+		}
+		break;
+	}
+	while (number != 1 || number != 0)
+	{
+		std::cout << "Please enter either a 1 or 0";
+	}
+}
+
+//find cost of repairs
+float A350IssueandSolutions::getCost() {
+	return cost;
+}
+
+//get time spent on repairs
+int A350IssueandSolutions::getTime() {
+	return time;
+}
