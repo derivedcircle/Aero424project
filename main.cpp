35e5fb79fc7ce9663dff154623e00f6bd5f232c4
AnshS <shahansh0509@gmail.com>
2024-04-12 13:47:24 -0400

    Added functionallity to have multiple play sessions and memory safety code Implimented a global and static varible

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
 //all standard libraries used
 #include "plane.h"
 #include "initializer.h"
@@ -12,16 +13,45 @@
 using namespace std;
 // setting default namespace as standard for convenience
 
+// high score global varible to keep track of the highest score earned in game
+int HIGH_SCORE_GV = 0;
 
 int main()
 {
-	UserInterface user1(initFun());
-	cout << user1 << endl;
-	user1.hangarOperation();
-	cout << user1 << endl;
-	user1.newAircraftArrivals();
-	user1.hangarOperation();
-	cout << user1 << endl;
-	
+	bool playAgain;
+	do// loop to play the game again
+	{
+		UserInterface user1(initFun());
+		cout << user1 << endl;
+		user1.hangarOperation();
+		cout << user1 << endl;
+		user1.newAircraftArrivals();
+		user1.hangarOperation();
+		cout << user1 << endl;
+
+		// game over code
+		if (user1.days > HIGH_SCORE_GV)
+		{
+			HIGH_SCORE_GV = user1.days;
+			cout << "You have just acheved the high score! Congradulations!" << endl;
+		}
+		cout << "Here are the names of those who have played before you" << endl;
+		for (auto& names : UserInterface::userHistory)
+		{
+			cout << names << endl;
+		}
+		string playGameAgainCheck;
+		
+		// the regex checks for yes/no or y/n with any capitalization
+		while (!regex_match(playGameAgainCheck, regex("^[y|Y][e|E][s|S]$|^[n|N][o|O]$|^[n|N]$|^[y|Y]$")))
+		{
+			// forces user to input a valid input for command before proceding
+			cout << "Would you like to play again? (Answer with a yes/no or y/n) ";
+			getline(cin, playGameAgainCheck);
+		}
+
+		// saves if the user wants to play the game again
+		playAgain = regex_match(playGameAgainCheck, regex("^[y|Y][e|E][s|S]$|^[y|Y]$"));
+	} while (playAgain);
 	return 0;
 }
--- userinterface.h
+++ userinterface.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 #include "plane.h"
 
 using namespace std;
@@ -15,13 +16,18 @@ class UserInterface
 	const double MAX_HANGAR_SPACE = 1000.0; // Can be adjusted - use in setting difficulty (may also be removed and added in plane class) (if so remove)
 	double hangarSpace;
 	float moneySpent;
-	int days;
+	
 	// a map with the planes in the hanger and how many days are remaining to do their maintanance
 	map<Plane*, int> planesInHangar;
+
+	
 	
 public:
+	int days;
 
 	bool hasPlaneCrashed;
+
+	static vector<string> userHistory; // a vector holding all of the former users who have played the game
 
 	UserInterface( string name);// UI constructor
 	
@@ -33,6 +39,8 @@ public:
 	void hangarOperation(); // function that goes through and decrements all aircraft maintanance times
 
 	Plane* createPlane(int planeid); // function that returns a plane pointer depending on the plane requested
+
+	~UserInterface();// UI Destructor
 };
 
 #endif // !UI_H
